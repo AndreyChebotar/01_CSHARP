5d63a764c8052c1c65fb8e5bd6175976c96bced0
AndreyChebotar <chebotar.andrey13@gmail.com>
2024-02-27 21:43:25 +0300

    add Lesson3:Task2

---
+++ Lesson3/Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿void PrintSqaures(int limit)
+{
+    int i = 1;
+    while(i <= limit)
+    {
+        Console.Write($"{i * i} ");
+        i = i + 1; // или можно использовать i++; 
+    }
+}
+PrintSqaures(5);
+PrintSqaures(10);
+PrintSqaures(15);
