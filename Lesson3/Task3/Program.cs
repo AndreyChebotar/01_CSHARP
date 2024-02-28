adb068d6ca390afed9d78e6b658d792ebe789d73
AndreyChebotar <chebotar.andrey13@gmail.com>
2024-02-28 18:21:01 +0300

    add Lesson3:Task3

---
+++ Lesson3/Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿void ZeroEvenElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+}
+
+int[] array = { 1, 2, 3, 4, 5 };
+ZeroEvenElements(array);
+PrintArray(array);
+
+
+//цикл foreach - не позволяет менять содержимое массива
+//цикл while - используется, когда нет четкого представления о числе операций
+//цикл for - используется для перебора элементов
